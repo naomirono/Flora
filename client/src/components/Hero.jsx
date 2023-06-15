c568214ed79229df5f1884d21422278fb54edeec
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-15 20:39:33 +0300

    Update hero section code

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import HeroVideo from '../assets/hero.mp4'
 
 const Hero = () => {
   return (
-    <section className="bg-green-900 py-24">
-      <div className="container mx-auto text-center">
-        <h1 className="text-white text-4xl font-bold mb-4">
-          Planting Trees for a Greener Future
-        </h1>
-        <p className="text-white text-lg mb-8">
-          Join us in our mission to restore the environment and combat climate change through tree planting initiatives.
-        </p>
-        <button className="bg-white text-green-900 rounded-md px-6 py-3 font-bold hover:bg-green-100">
-          Get Involved
-        </button>
-      </div>
-    </section>
+    <section className="relative h-[89vh] flex items-center justify-center">
+    <div className="video-container absolute top-0 left-0 w-full h-full">
+      <video className="w-full h-full object-cover" autoPlay muted loop>
+        <source src={HeroVideo} type="video/mp4" />
+      </video>
+    </div>
+    <div className="container mx-auto text-center relative z-10">
+      <h1 className="text-white text-4xl font-bold mb-4">
+        Planting Trees for a Greener Future
+      </h1>
+      <p className="text-white text-lg mb-8">
+        Join us in our mission to restore the environment and combat climate change through tree planting initiatives.
+      </p>
+      <button className="bg-white text-green-900 rounded-md px-6 py-3 font-bold hover:bg-green-100">
+        Get Involved
+      </button>
+    </div>
+  </section>
+  
+
   );
 };
 
