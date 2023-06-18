f65a00b90c68ae14532d7f63410234d7f7011fa1
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-18 20:39:06 +0300

    Create a donate component from the CallToAction component

---
+++ client/src/components/Donate.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CallToAction from './CallToAction';
+
+const Donate = () => {
+  return (
+    <div>
+      
+      <CallToAction
+        title="Donate to Support Tree Planting"
+        description="Your contribution can make a difference in restoring our planet. Help us plant more trees and combat climate change."
+        buttonText="Donate Now"
+      />
+
+    </div>
+  );
+};
+
+export default Donate;
