ba92af3bbdd4a320eba11ac4d714393bd4f418d3
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-18 14:51:10 +0300

    create a mission component and add code

---
+++ client/src/components/Mission.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Mission = () => {
+  return (
+    <section className="py-12">
+      <div className="container mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+          <p className="text-lg">
+            At our organization, we are committed to making a lasting impact on the environment and communities through tree planting initiatives. We firmly believe that trees play a crucial role in combatting climate change, restoring habitats, and promoting a sustainable future. Through our dedicated efforts, we aim to create greener and healthier environments, improve air and water quality, mitigate the effects of deforestation, and enhance biodiversity. By planting trees, we empower individuals and communities to actively contribute to the preservation of our planet and build a better world for future generations.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Mission;
