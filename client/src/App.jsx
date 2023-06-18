04969569ac887bfd3fe483c4f1ab07dd5834ef34
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-18 14:48:56 +0300

    Import About section and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import About from './components/About';
 
 const App = () => {
   return (
-    <>
+    <> 
+    <div className='bg-green-100'>
       <Header/>
       <Hero/>
+      <About/>
+    </div>
     </>
   )
 }
