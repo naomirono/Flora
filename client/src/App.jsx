72f0518961c473134a82e8b0161dbe436ce662af
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-18 20:50:08 +0300

    Import Donate component and render it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Mission from './components/Mission';
 import Projects from './components/Project';
+import Donate from './components/Donate';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <About/>
       <Mission/>
       <Projects/>
+      <Donate/>
     </div>
     </>
   )
