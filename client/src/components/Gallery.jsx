45d312d1db5179afa4e57a8c479724668523fb08
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-21 19:23:41 +0300

    Update Gallery component for responsive image display with media queries

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import '../gallery.css'
-import backgroundImage from '../assets/hero1.png'
-import Image1 from '../assets/p1.jpg'
-import Image2 from '../assets/p2.png'
-import Image3 from '../assets/p4.JPG'
-import Image4 from '../assets/p3.JPG'
-import Image5 from '../assets/p5.JPG'
+import { useMediaQuery } from 'react-responsive';
+import '../gallery.css';
+import Image1 from '../assets/p1.jpg';
+import Image2 from '../assets/p2.png';
+import Image3 from '../assets/p4.JPG';
+import Image4 from '../assets/p3.JPG';
+import Image5 from '../assets/p5.JPG';
 
 const Gallery = () => {
-    return (
-      <div className="gallery py-4">
-        <img src={Image1} alt="Image 1"  />
-        <img src={Image2} alt="Image 2" />
-        <img src={Image3} alt="Image 3"/>
-        <img src={Image4} alt="Image 4" />
-        <img src={Image5} alt="Image 5"/>
-      </div>
-    );
-  };
+  const isSmallDevice = useMediaQuery({ maxWidth: 767 }); // Define the breakpoint for small devices
 
-  export default Gallery;
+  const images = isSmallDevice
+    ? [Image1, Image2, Image4] // Display 3 images on small devices
+    : [Image1, Image2, Image3, Image4, Image5]; // Display all 5 images on medium and larger devices
+
+  return (
+    <div className="gallery py-4 px-2 md:px-4">
+      {images.map((image, index) => (
+        <img key={index} src={image} alt={`Image ${index + 1}`} />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
