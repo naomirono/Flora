f6eac4e0fdea022fb9647a77b2585bda8a22636d
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-21 19:31:26 +0300

    Adjust the height on small devices

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import backgroundImage from '../assets/hero1.png'
+import backgroundImage from '../assets/hero1.png';
 
 const Hero = () => {
   return (
-    <section id="home" className="relative h-[89vh] flex items-center justify-center">
+    <section id="home" className="relative h-[70vh] md:h-[89vh] flex items-center justify-center">
       <div className="background-image absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
       <div className="container mx-auto text-center relative z-10">
