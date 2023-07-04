7c055718406e2709e825370743765a11e633ce12
Naomi Cherotich <naomirono1454@gmail.com>
2023-07-04 21:47:58 +0300

    add blogData.js file in the data folder to store blog post information

---
+++ client/src/data/NewsData.js
@@ -0,0 +1,23 @@
+const newsData = [
+    {
+      title: 'Title of Blog Post 1',
+      date: 'January 1, 2023',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
+      image: require('../assets/hero1.png').default,
+    },
+    {
+      title: 'Title of Blog Post 2',
+      date: 'February 15, 2023',
+      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
+      image: require('../assets/hero1.png').default,
+    },
+    {
+        title: 'Title of Blog Post 2',
+        date: 'February 15, 2023',
+        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
+        image: require('../assets/hero1.png').default,
+      },
+  ];
+  
+  export default newsData;
+  
