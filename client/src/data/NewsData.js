928ebe80a914492a047f94b859ff6cb18b8f5661
Naomi Cherotich <naomirono1454@gmail.com>
2023-07-05 12:06:31 +0300

    update image import

--- client/src/data/NewsData.js
+++ client/src/data/NewsData.js
@@ -1,23 +1,25 @@
-const newsData = [
-    {
-      title: 'Title of Blog Post 1',
-      date: 'January 1, 2023',
-      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
-      image: require('../assets/hero1.png').default,
-    },
-    {
-      title: 'Title of Blog Post 2',
-      date: 'February 15, 2023',
-      excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
-      image: require('../assets/hero1.png').default,
-    },
-    {
-        title: 'Title of Blog Post 2',
-        date: 'February 15, 2023',
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae risus vitae tellus congue fringilla.',
-        image: require('../assets/hero1.png').default,
-      },
-  ];
-  
-  export default newsData;
-  
+import image1 from '../assets/hero1.png';
+
+const blogData = [
+  {
+    title: '10 Tips for Sustainable Living',
+    date: 'January 1, 2023',
+    excerpt: 'Check out these 10 practical tips for incorporating sustainable practices into your daily life and reducing your environmental footprint.',
+    image: image1,
+  },
+  {
+    title: 'The Importance of Tree Planting in Urban Areas',
+    date: 'February 15, 2023',
+    excerpt: 'Learn about the benefits of urban reforestation and how planting trees in cities can improve air quality, reduce heat islands, and enhance community well-being.',
+    image: image1,
+  },
+  {
+    title: 'How Trees Help Combat Climate Change',
+    date: 'March 20, 2023',
+    excerpt: 'Discover the vital role of trees in mitigating climate change, including their ability to sequester carbon dioxide and contribute to the overall health of our planet.',
+    image: image1,
+  },
+  // Add more blog post data as needed
+];
+
+export default blogData;
