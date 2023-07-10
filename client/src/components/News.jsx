009a7700d68142a2810691ccf39ed4ca8f235c37
Naomi Cherotich <naomirono1454@gmail.com>
2023-07-10 22:33:05 +0300

    Rename title

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -6,7 +6,7 @@ const News = () => {
   return (
     <section className="py-12">
       <div className="container mx-auto max-w-[1100px]">
-        <h2 className="text-4xl font-bold mb-8 text-center">Blog</h2>
+        <h2 className="text-4xl font-bold mb-8 text-center">Blog And News</h2>
         <div className="grid gap-8 md:grid-cols-3">
           {newsData.map((post, index) => (
             <NewsPost key={index} post={post} />
