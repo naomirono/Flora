b92aa21176f94e8132448fa4f9972d522cfa15f8
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-02 23:32:49 +0300

    update code

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -17,11 +17,11 @@ const ContactSection = () => {
             <h2 className="text-3xl md:text-4xl font-bold mb-4">Contact Us</h2>
             <p className="text-gray-800 mb-6 pr-0 sm:pr-10">Have questions or want to get involved? We'd love to hear from you. Reach out to us using the contact details below or fill out the form and we'll get back to you as soon as possible.</p>
             <ul className="text-gray-800">
-              <li className="mb-2"><FaEnvelope className="inline-block mr-2 text-[#7B1FA2]" /> Email: info@carecycle.org</li>
+              <li className="mb-2"><FaEnvelope className="inline-block mr-2 text-[#7B1FA2]" /> Email: info@TreeHaven.org</li>
               <li className="mb-2"><FaPhoneAlt className="inline-block mr-2 text-[#7B1FA2]" /> Phone: +1234567890</li>
-              <li className="mb-2"><FaTwitter className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Twitter: @carecycle</li>
-              <li className="mb-2"><FaFacebook className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Facebook: @carecycle</li>
-              <li><FaInstagram className="inline-block mr-2 text-pink-500 hover:text-pink-700 cursor-pointer" /> Instagram: @carecycle</li>
+              <li className="mb-2"><FaTwitter className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Twitter: @TreeHaven</li>
+              <li className="mb-2"><FaFacebook className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Facebook: @TreeHaven</li>
+              <li><FaInstagram className="inline-block mr-2 text-pink-500 hover:text-pink-700 cursor-pointer" /> Instagram: @Treehaven</li>
             </ul>
           </div>
           <div className="md:w-1/2">
