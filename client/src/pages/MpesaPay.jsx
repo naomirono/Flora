3ea364c4dc102af2d698463e31890c7666c4c90f
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-10 20:36:39 +0300

    create a mpesapay component to handle mpesa donation payments

---
+++ client/src/pages/MpesaPay.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const MpesaPay = () => {
+    const [amount, setAmount] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [paymentResponse, setPaymentResponse] = useState(null);
+
+    const handlePayment = async () => {
+        try {
+            const response = await axios.post('/initiate-payment', {
+                amount,
+                phoneNumber
+            });
+
+            setPaymentResponse(response.data);
+        } catch (error) {
+            console.error('Error initiating payment:', error);
+        }
+    };
+
+    return (
+        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h2 className="text-xl font-semibold mb-4">M-Pesa Payment Integration</h2>
+                <div className="mb-4">
+                    <label className="block text-sm font-medium">Amount:</label>
+                    <input
+                        type="number"
+                        className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-sm font-medium">Phone Number:</label>
+                    <input
+                        type="text"
+                        className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                </div>
+                <button
+                    className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300"
+                    onClick={handlePayment}
+                >
+                    M-Pesa Pay
+                </button>
+                {paymentResponse && (
+                    <div className="mt-4">
+                        <h3 className="text-lg font-semibold mb-2">Payment Response:</h3>
+                        <pre className="bg-gray-200 p-4 rounded">{JSON.stringify(paymentResponse, null, 2)}</pre>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default MpesaPay;
