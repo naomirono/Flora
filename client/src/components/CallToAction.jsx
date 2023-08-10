abd1528219edeab6ad167fcaada2de2244fd9ff0
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-10 20:34:28 +0300

    Add a text value to the donate button

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -7,7 +7,7 @@ const CallToAction = ({ title, description }) => {
       <div className="container mx-auto px-4 max-w-[1100px] text-center relative z-10">
         <h2 className="text-2xl text-white font-semibold mb-4">{title}</h2>
         <p className="text-white mb-4">{description}</p>
-        <DonateButton/>
+        <DonateButton text="Donate Now"/>
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60"></div>
     </div>
