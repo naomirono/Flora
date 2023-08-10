d31d4e00dc8b71d6619442bf2c5491ba18673d73
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-10 22:25:40 +0300

    Add opacity to the background image and position for the mpesa intergration

--- client/src/pages/MpesaPay.jsx
+++ client/src/pages/MpesaPay.jsx
@@ -22,46 +22,48 @@ const MpesaPay = () => {
 
     return (
         <div
-        className="bg-black min-h-screen flex items-center justify-center"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          opacity: 0.8, // Set the desired opacity
-        }}
-      >
-            <div className="bg-white p-8 rounded shadow-md w-96">
-                <h2 className="text-xl font-semibold mb-4">M-Pesa Payment Integration</h2>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium">Amount:</label>
-                    <input
-                        type="number"
-                        className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium">Phone Number:</label>
-                    <input
-                        type="text"
-                        className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-                </div>
-                <button
-                    className="w-full bg-[#006600] text-white py-2 rounded hover:bg-blue-600 transition duration-300"
-                    onClick={handlePayment}
-                >
-                    M-Pesa Pay
-                </button>
-                {paymentResponse && (
-                    <div className="mt-4">
-                        <h3 className="text-lg font-semibold mb-2">Payment Response:</h3>
-                        <pre className="bg-gray-200 p-4 rounded">{JSON.stringify(paymentResponse, null, 2)}</pre>
+            className=" relative min-h-screen flex items-center justify-center"
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <div className="overlay absolute inset-0 bg-black opacity-60"></div>
+            <div className="container mx-auto flex items-center justify-center">
+                <div className="bg-white p-8 rounded shadow-md w-96 relative">
+                    <h2 className="text-xl font-semibold mb-4">M-Pesa Payment Integration</h2>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Amount:</label>
+                        <input
+                            type="number"
+                            className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                        />
                     </div>
-                )}
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium">Phone Number:</label>
+                        <input
+                            type="text"
+                            className="w-full border rounded px-3 py-2 outline-none focus:border-blue-500"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        className="w-full bg-[#006600] text-white py-2 rounded hover:bg-blue-600 transition duration-300"
+                        onClick={handlePayment}
+                    >
+                        M-Pesa Pay
+                    </button>
+                    {paymentResponse && (
+                        <div className="mt-4">
+                            <h3 className="text-lg font-semibold mb-2">Payment Response:</h3>
+                            <pre className="bg-gray-200 p-4 rounded">{JSON.stringify(paymentResponse, null, 2)}</pre>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
