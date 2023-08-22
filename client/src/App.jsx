a3f2b71ff634d0d49d0d15eda6bc1a239505c37e
Naomi Cherotich <naomirono1454@gmail.com>
2023-08-22 20:58:23 +0300

    set up routes inside app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -9,24 +10,28 @@ import TreeStatistics from './components/TreeStatistics';
 import News from './components/News';
 import ContactSection from './components/Contact';
 import Footer from './components/Footer';
+import MpesaPay from './pages/MpesaPay';
 
 const App = () => {
   return (
-    <> 
-    <div className='bg-green-100'>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Mission/>
-      <Projects/>
-      <Donate/>
-      <TreeStatistics/>
-      <News/>
-      <ContactSection/>
-      <Footer/>
-    </div>
-    </>
-  )
-}
+    <Router>
+      <div className='bg-green-100'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/statistics" element={<TreeStatistics />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<ContactSection />} />
+          <Route path="/mpesapay" element={<MpesaPay />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
