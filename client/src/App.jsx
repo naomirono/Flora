945c8bbfa6bdd4518661bb59e83f44dc9a6d172f
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-29 18:40:53 +0300

    Remove routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -10,27 +9,21 @@ import TreeStatistics from './components/TreeStatistics';
 import News from './components/News';
 import ContactSection from './components/Contact';
 import Footer from './components/Footer';
-import MpesaPay from './pages/MpesaPay';
 
 const App = () => {
   return (
-    <Router>
-      <div className='bg-green-100'>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/mission" element={<Mission />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/statistics" element={<TreeStatistics />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/contact" element={<ContactSection />} />
-          <Route path="/mpesapay" element={<MpesaPay />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className='bg-green-100'>
+      <Header />
+      <Hero />
+      <About />
+      <Mission />
+      <Projects />
+      <Donate />
+      <TreeStatistics />
+      <News />
+      <ContactSection />
+      <Footer />
+    </div>
   );
 };
 
